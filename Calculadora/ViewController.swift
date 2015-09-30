1358f0badb55b4513a00f5e2d975605b7f27ebbe
Giovanni Bonetti <giovanni.bonetti@gmail.com>
2015-09-29 22:21:52 -0300

    Calculadora - digitos funcionando

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+
+    var isTyping = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        display.text = "0"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func appendDigit(sender: UIButton) {
+        let currentDigit = sender.currentTitle!
+
+        if isTyping {
+            display.text = display.text! + currentDigit
+        } else {
+            display.text = currentDigit
+            isTyping = true
+        }
+    }
 
 }
 
